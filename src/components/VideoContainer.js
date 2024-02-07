cef6d143463aa01df38bf57efae1cb3fdd4d82f6
= <harmostparmod@gmail.com>
2024-02-07 08:49:34 +0530

    Youtube api data fetch && built the main container

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import { YOUTUBE_VIDEOS_API } from "../utils/constants";
+import VideoCard from "./VideoCard";
 
 const VideoContainer = () => {
-  return <div>VideoContainer</div>;
+  const [videos, setVideos] = useState([]);
+
+  const getVideos = async () => {
+    const data = await fetch(YOUTUBE_VIDEOS_API);
+    const json = await data.json();
+    console.log("yt videos reult  : ", json);
+    setVideos(json?.items);
+  };
+
+  useEffect(() => {
+    getVideos();
+  }, []);
+
+  return (
+    <div className="flex flex-wrap">
+      {videos.map((video) => (
+        <VideoCard key={video.id} info={video} />
+      ))}
+    </div>
+  );
 };
 
 export default VideoContainer;
